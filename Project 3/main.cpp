daa51673315e6e40a52185482252a352809ded5d
Paul Casperson <paulcasperson@pauls-macbook-pro.local>
2016-11-06 17:24:37 -0700

    Initial Commit

---
+++ Project 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 3
+//
+//  Created by Paul Casperson on 11/6/16.
+//  Copyright © 2016 Dhalton Huber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
